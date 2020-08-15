20669376c6064776c8ffa9e5b01ca3d178b036be
Edgar Kenyi Rojo Moreno <edgar.kenyi.rojo@gmail.com>
2020-08-14 21:48:25 -0500

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../api/youtube";
-import VideoList from "./VideoList"
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 const KEY = "AIzaSyBhIXVcXyRadqS-32MCeZ7AYCgfiXAy1RI";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
@@ -21,11 +22,20 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}  />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
+        
       </div>
     );
   }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,15 +1,19 @@
-import "./VideoItem.css"
+import "./VideoItem.css";
 import React from "react";
 
-
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className="video-item item">
+    <div
+      onClick={() => {
+        onVideoSelect(video);
+      }}
+      className="video-item item"
+    >
       <img
         className="ui image"
         src={video.snippet.thumbnails.medium.url}
         alt={video.snippet.description}
-        //   key={video.id.videoId}
+        key={video.id.videoId}
       />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
